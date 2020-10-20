a700801625b6bcf1d858da64f3f8d8240a7e512c
yuzilan <47586840+yuzilan@users.noreply.github.com>
2020-10-20 18:50:00 +0800

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -12,14 +12,28 @@ def argparser():
     # parser.add_argument('--num_epoch', type=int, help='Number of epochs to train.', default=100)
 
     # params for train
-    parser.add_argument('--train_outputs', type=str, help='Path to save training outputs.', default='./datasets/train_outputs/')
+    parser.add_argument('--train_outputs', type=str, help='Path to save training outputs.', default='./train_outputs/')
     parser.add_argument('--restore_model_path', type=str, help='A history model you have trained, you can load it and continue trainging.', default='')
     parser.add_argument('--batch_size', type=int, help='Must divide evenly into the dataset sizes.', default=4)
     parser.add_argument('--num_classes', type=int, help='Num of classes which your task should classify', default=40)
     parser.add_argument('--input_size', type=int, help='The input image size of the model', default=456)
     parser.add_argument('--learning_rate', type=float, help='Initial learning rate.', default=0.0001)
-    parser.add_argument('--max_epochs', type=int, default=30)
+    parser.add_argument('--max_epochs', type=int, default=6)
 
+    # params for save pb
+    parser.add_argument('--test_data_url', type=str, help='the test data path on obs', default='./datasets/test_data/')
+    parser.add_argument('--keep_weights_file_num', type=int, help='the max num of weights files keeps, if set -1, means infinity', default=20)
+    
+#     tf.app.flags.DEFINE_string('test_data_url', '', 'the test data path on obs')
+#     tf.app.flags.DEFINE_string('deploy_script_path', '',
+#                            'a path which contain config.json and customize_service.py, '
+#                            'if it is set, these two scripts will be copied to {train_url}/model directory')
+# tf.app.flags.DEFINE_string('freeze_weights_file_path', '',
+#                            'if it is set, the specified h5 weights file will be converted as a pb model, '
+#                            'only valid when {mode}=save_pb')
+    
+#     tf.app.flags.DEFINE_integer('keep_weights_file_num', 20,
+#                             'the max num of weights files keeps, if set -1, means infinity')
     # tf.app.flags.DEFINE_string('data_url', '', 'the training data path')
     # # tf.app.flags.DEFINE_string('train_url', '', 'the path to save training outputs')
     # tf.app.flags.DEFINE_integer('keep_weights_file_num', 20,
