91399a0a19f93a9e9e778592f840ab1cdfbdd6f7
yuzilan <47586840+yuzilan@users.noreply.github.com>
2020-12-25 21:30:04 +0800

    Delete Smooth_labels.py

--- tools/Smooth_labels.py
+++
@@ -1,9 +0,0 @@
-# # 标签平滑
-# def smooth_labels(y, smooth_factor=0.1):
-#     assert len(y.shape) == 2
-#     if 0 <= smooth_factor <= 1:
-#         y *= 1 - smooth_factor
-#         y += smooth_factor / y.shape[1]
-#     else:
-#         raise Exception('Invalid label smoothing factor: ' + str(smooth_factor))
-#     return y
